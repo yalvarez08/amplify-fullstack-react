d0f8ae46117e37f43dfd757c85f2bfb6a34b1324
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-04-01 09:56:04 -0400

    aws auth now working with app login

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyfullstackreac8c7554c3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  Button,
+  Card,
+  Heading,
+  View,
+  withAuthenticator,
+  Image,
+} from "@aws-amplify/ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
-    </div>
+function App({signOut}) {
+  return(
+  <View className="App">
+    <Card>
+      <Image src={logo} className="App-logo" alt="logo" />
+      <Heading level={1}>We now have Auth!</Heading>
+    </Card>
+      
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
-}
+};
 
-export default App;
+export default withAuthenticator(App);
